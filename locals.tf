3dd38b862c7a6213e0c8959b95ae9d60767b0c4e
Olamide Olaleye <olamide.olaleye@outlook.com>
2024-04-20 08:58:40 +0100

    update:removed linux machines configurations and resized the disks for windows machine to 60G.

--- locals.tf
+++ locals.tf
@@ -1,41 +1,6 @@
 locals {
   vm_configs = [
     {
-      name        = "prod-ubuntu-22.04.4"
-      tags        = "prod,linux"
-      target_node = "pve"
-      vmid        = 8000
-      memory      = 4096
-      cores       = 2
-      sockets     = 1
-      agent       = 1
-      bios        = "ovmf"
-      os_type     = "ubuntu 22.04.3"
-      scsihw      = "virtio-scsi-pci"
-      qemu_os     = "l26"
-      cpu         = "x86-64-v2-AES"
-      vcpus       = 2
-      iso         = "local:iso/ubuntu-22.04.3-live-server-amd64.iso"
-      onboot      = false
-      oncreate    = false
-      numa        = true
-      hotplug     = "network,disk,memory,cpu"
-      machine     = "q35"
-      bootdisk    = "scsi0"
-      network = {
-        model  = "virtio"
-        bridge = "vmbr1"
-        tag    = 7
-      }
-      disk = {
-        type    = "scsi"
-        size    = "50G"
-        storage = "local-lvm"
-        discard = "on"
-        ssd     = 1
-      }
-    },
-    {
       name        = "prod-winserver-2022-core"
       tags        = "prod, windows"
       target_node = "pve"
@@ -63,7 +28,7 @@ locals {
       }
       disk = {
         type    = "scsi"
-        size    = "100G"
+        size    = "60G"
         storage = "local-lvm"
         discard = "on"
         ssd     = 1
@@ -98,41 +63,6 @@ locals {
       disk = {
         type    = "scsi"
         size    = "100G"
-        storage = "local-lvm"
-        discard = "on"
-        ssd     = 1
-      }
-    },
-    {
-      name        = "prod-rocky-linux-9.3"
-      tags        = "prod,linux"
-      target_node = "pve"
-      vmid        = 8003
-      memory      = 4096
-      cores       = 2
-      sockets     = 1
-      agent       = 1
-      bios        = "ovmf"
-      os_type     = "rocky 9.3"
-      scsihw      = "virtio-scsi-pci"
-      qemu_os     = "l26"
-      cpu         = "x86-64-v2-AES"
-      vcpus       = 2
-      iso         = "local:iso/Rocky-9.3-x86_64-dvd.iso"
-      onboot      = false
-      oncreate    = false
-      numa        = true
-      hotplug     = "network,disk,memory,cpu"
-      machine     = "q35"
-      bootdisk    = "scsi0"
-      network = {
-        model  = "virtio"
-        bridge = "vmbr1"
-        tag    = 7
-      }
-      disk = {
-        type    = "scsi"
-        size    = "50G"
         storage = "local-lvm"
         discard = "on"
         ssd     = 1
