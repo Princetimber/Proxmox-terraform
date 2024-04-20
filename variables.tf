3bef96bc5d3e9982c581e4de6177adee18197b06
Olamide Olaleye <olamide.olaleye@outlook.com>
2024-04-20 08:59:54 +0100

    update:removed linux machines configurations and resized the disks for windows machine to 60G.

--- variables.tf
+++ variables.tf
@@ -1,39 +1,6 @@
 variable "local_vm_configs" {
   default = [
-    {
-      name        = "prod-ubuntu-22.04.4"
-      tags        = "prod,linux"
-      target_node = "pve"
-      vmid        = 8000
-      memory      = 4096
-      cores       = 2
-      sockets     = 1
-      agent       = 1
-      bios        = "ovmf"
-      os_type     = "ubuntu 22.04.3"
-      scsihw      = "virtio-scsi-pci"
-      qemu_os     = "l26"
-      cpu         = "x86-64-v2-AES"
-      vcpus       = 2
-      iso         = "local:iso/ubuntu-22.04.3-live-server-amd64.iso"
-      onboot      = false
-      oncreate    = false
-      numa        = true
-      hotplug     = "network,disk,memory,cpu"
-      machine     = "q35"
-      bootdisk    = "scsi0"
-      network = {
-        model  = "virtio"
-        bridge = "vmbr1"
-      }
-      disk = {
-        type    = "scsi"
-        size    = "50G"
-        storage = "local-lvm"
-        discard = "on"
-        ssd     = 1
-      }
-    },
+
     {
       name        = "prod-winserver-2022-gui"
       tags        = "prod, windows"
@@ -97,42 +64,7 @@ variable "local_vm_configs" {
       }
       disk = {
         type    = "scsi"
-        size    = "100G"
-        storage = "local-lvm"
-        discard = "on"
-        ssd     = 1
-      }
-    },
-    {
-      name        = "prod-rocky-linux-9.3"
-      tags        = "prod,linux"
-      target_node = "pve"
-      vmid        = 8003
-      memory      = 4096
-      cores       = 2
-      sockets     = 1
-      agent       = 1
-      bios        = "ovmf"
-      os_type     = "rocky 9.3"
-      scsihw      = "virtio-scsi-pci"
-      qemu_os     = "l26"
-      cpu         = "x86-64-v2-AES"
-      vcpus       = 2
-      iso         = "local:iso/Rocky-9.3-x86_64-dvd.iso"
-      onboot      = false
-      oncreate    = false
-      numa        = true
-      hotplug     = "network,disk,memory,cpu"
-      machine     = "q35"
-      bootdisk    = "scsi0"
-      network = {
-        model  = "virtio"
-        bridge = "vmbr1"
-        tag    = 7
-      }
-      disk = {
-        type    = "scsi"
-        size    = "50G"
+        size    = "60G"
         storage = "local-lvm"
         discard = "on"
         ssd     = 1
