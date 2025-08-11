c94daf7453cc3f7b7ff5fd3eefaec6857d13fb9a
Olamide Olaleye <olamide.olaleye@outlook.com>
2025-08-11 17:01:42 +0100

    configuration for BpG

--- main.tf
+++ main.tf
@@ -1,56 +1,62 @@
-resource "proxmox_vm_qemu" "vm_template" {
+resource "proxmox_virtual_environment_vm" "vm_template" {
   for_each    = { for vm_config in var.local_vm_configs : vm_config.name => vm_config }
   name        = each.value.name
-  tags        = each.value.tags
-  target_node = each.value.target_node
-  vmid        = each.value.vmid
-  memory      = each.value.memory
-  cores       = each.value.cores
-  sockets     = each.value.sockets
-  agent       = each.value.agent
-  bios        = each.value.bios
-  os_type     = each.value.os_type
-  scsihw      = each.value.scsihw
-  qemu_os     = each.value.qemu_os
-  cpu         = each.value.cpu
-  vcpus       = each.value.vcpus
-  iso         = each.value.iso
-  onboot      = each.value.onboot
-  oncreate    = each.value.oncreate
-  numa        = each.value.numa
-  hotplug     = each.value.hotplug
-  machine     = each.value.machine
-  bootdisk    = each.value.bootdisk
-  network {
-    model  = each.value.network.model
-    bridge = each.value.network.bridge
-    tag    = try(each.value.network.tag, -1)
+  description = "Managed by Terraform"
+  tags        = split(",", each.value.tags)
+  node_name   = each.value.target_node
+  vm_id       = each.value.vmid
+  on_boot     = each.value.onboot
+
+  cpu {
+    cores   = each.value.cores
+    sockets = each.value.sockets
+    type    = each.value.cpu
   }
+
+  memory {
+    dedicated = each.value.memory
+  }
+
+  agent {
+    enabled = each.value.agent == 1
+  }
+
+  bios = each.value.bios
+
+  operating_system {
+    type = each.value.os_type
+  }
+
+  network_device {
+    model   = each.value.network.model
+    bridge  = each.value.network.bridge
+    vlan_id = try(each.value.network.tag, -1)
+  }
+
   disk {
-    type    = each.value.disk.type
-    size    = each.value.disk.size
-    storage = each.value.disk.storage
-    discard = each.value.disk.discard
-    ssd     = each.value.disk.ssd
+    datastore_id = each.value.disk.storage
+    file_format  = "raw" # Assuming raw, as type was 'scsi'
+    interface    = "scsi0"
+    size         = each.value.disk.size
+    ssd          = each.value.disk.ssd == 1
   }
 
-  dynamic "disk" {
-    for_each = can(each.value.virtio_iso) ? [1] : []
-    content {
-      type    = "ide"
-      media   = "cdrom"
-      file    = each.value.virtio_iso
-      storage = each.value.disk.storage
-      size    = "1G" # Default size for a CD-ROM
-    }
+  disk {
+    datastore_id = each.value.disk.storage
+    file_format  = "raw"
+    interface    = "efidisk0"
+    size         = 4
   }
 
-  dynamic "disk" {
-    for_each = each.value.bios == "ovmf" ? [1] : []
-    content {
-      type    = "efidisk"
-      storage = each.value.disk.storage
-      size    = "4M"
-    }
+  cdrom {
+    file_id = each.value.iso
+  }
+
+  disk {
+    datastore_id      = "local" # ISOs are usually on 'local' storage
+    file_format       = "raw"
+    interface         = "ide3"
+    path_in_datastore = each.value.virtio_iso
+    size              = 1 # Size is required but ignored for ISOs
   }
 }
